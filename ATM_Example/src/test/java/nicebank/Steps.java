7adead58f5462bc247c8c96afaec643cfa82145d
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-10-07 11:17:34 -0400

    Given i have deposited implemented with Account class stub.

--- ATM_Example/src/test/java/nicebank/Steps.java
+++ ATM_Example/src/test/java/nicebank/Steps.java
@@ -6,10 +6,16 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Steps {
+	
+	class Account {
+		public Account(int openingBalance) {
+			
+		}
+	}
+	
 	@Given("^I have deposited \\$(\\d+) in my account$")
-	public void iHaveDeposited$InMyAccount(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void iHaveDeposited$InMyAccount(int amount) throws Throwable {
+	    new Account(amount);
 	}
 
 	@When("^I request \\$(\\d+)$")
