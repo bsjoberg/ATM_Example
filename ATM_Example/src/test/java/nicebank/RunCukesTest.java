f121a7969e59cf9df464ce838d2e00366eda9d05
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-10-07 19:41:13 -0400

    Formatting

--- ATM_Example/src/test/java/nicebank/RunCukesTest.java
+++ ATM_Example/src/test/java/nicebank/RunCukesTest.java
@@ -6,14 +6,13 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
-	@RunWith(Cucumber.class)
-	@CucumberOptions(
-			plugin = {"pretty", "html:out"} ,
-			features = "src/test/resources" ,
-			glue = { "nicebank", "hooks", "support"} ,
-			snippets = SnippetType.CAMELCASE
-			)
-	
-	public class RunCukesTest {
-	
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = { "pretty", "html:out" }, 
+		features = "src/test/resources", 
+		glue = { "nicebank", "hooks", "support" }, 
+		snippets = SnippetType.CAMELCASE)
+
+public class RunCukesTest {
+
 }
