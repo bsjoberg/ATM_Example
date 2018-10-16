007380925465b7ada2b14d6790160cbd31517375
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-10-16 17:11:28 -0400

    Simple add money unit test

--- ATM_Example/src/test/java/nicebank/MoneyUnit.java
+++ ATM_Example/src/test/java/nicebank/MoneyUnit.java
@@ -11,5 +11,12 @@ public class MoneyUnit {
 		Assert.assertEquals(100, money.dollars());
 		Assert.assertEquals(50, money.cents());
 	}
-
+	
+	@Test
+	public void testAddMoney() {
+		Money money = new Money("$50.49");
+		money = money.add(new Money("$20.10"));
+		
+		Assert.assertTrue(money.equals(new Money("$70.59")));
+	}
 }
