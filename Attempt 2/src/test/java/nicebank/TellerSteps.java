2ddecf1e2171e6ba0452d2454a7711ef521b2894
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-11-02 18:32:12 -0400

    Notified of insufficient funds.

--- Attempt 2/src/test/java/nicebank/TellerSteps.java
+++ Attempt 2/src/test/java/nicebank/TellerSteps.java
@@ -1,12 +1,14 @@
 package nicebank;
 
-import cucumber.api.PendingException;
+import org.junit.Assert;
+
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import support.KnowsTheDomain;
 
 public class TellerSteps {
 	KnowsTheDomain helper;
+	boolean sufficientFunds = true;
 	
 	public TellerSteps(KnowsTheDomain helper) {
 		this.helper = helper;
@@ -15,12 +17,11 @@ public class TellerSteps {
 	@When("^I withdraw \\$(\\d+)$")
 	public void iRequest$(int amount) throws Throwable {
 	    Teller teller = new Teller(helper.getCashSlot());
-	    teller.withdrawFrom(helper.getMyAccount(), amount);
+	    sufficientFunds = teller.withdrawFrom(helper.getMyAccount(), amount);
 	}
 	
 	@Then("^I should be notified of insufficient funds$")
 	public void iShouldBeNotifiedOfInsufficientFunds() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    Assert.assertFalse(sufficientFunds);
 	}
 }
