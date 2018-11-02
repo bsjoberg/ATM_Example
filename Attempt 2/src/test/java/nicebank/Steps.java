186e062c3fe29901944e58afbfd035975f80fecf
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-11-02 13:09:41 -0400

    Added Money class.

--- Attempt 2/src/test/java/nicebank/Steps.java
+++ Attempt 2/src/test/java/nicebank/Steps.java
@@ -1,21 +1,33 @@
 package nicebank;
 
+import org.junit.Assert;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+
 public class Steps {
 	
 	class Account {
-		public void deposit (int amount) {
+		private Money balance = new Money();
+		
+		public void deposit (Money amount) {
+			balance = balance.add(amount);
+		}
+		
+		public Money getBalance() {
+			return balance;
 		}
 	}
 	
-	@Given("^I have deposited \\$(\\d+) in my account$")
-	public void iHaveDeposited$InMyAccount(int amount) throws Throwable {
+	@Given("^I have deposited \\$(\\d+)\\.(\\d+) in my account$")
+	public void iHaveDeposited$InMyAccount(int dollars, int cents) throws Throwable {
 	    Account myAccount = new Account();
+	    Money amount = new Money(dollars, cents);
 	    myAccount.deposit(amount);
+	    
+	    Assert.assertEquals("Incorrect account balance - ", amount, myAccount.getBalance());
 	}
 
 	@When("^I request \\$(\\d+)$")
