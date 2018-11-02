41bb107521416850c5a3e2b41b4d0d32bcf2584a
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-11-02 12:37:06 -0400

    Inital step definitions and runner class.

---
+++ Attempt 2/src/test/java/nicebank/RunCukesTest.java
@@ -0,0 +1,18 @@
+package nicebank;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = { "pretty", "html:out" }, 
+		features = "src/test/resources", 
+		glue = { "nicebank", "hooks", "support" }, 
+		snippets = SnippetType.CAMELCASE)
+
+public class RunCukesTest {
+
+}
