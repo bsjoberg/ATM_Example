9816f78bc198e67f7a0a137f95a2094a220d3fd4
Brian Sjoberg <brian_sjoberg@yahoo.com>
2018-11-03 10:39:36 -0400

    Couple quick unit tests for deposit and debit.

---
+++ Attempt 2/src/test/java/nicebank/AccountUnit.java
@@ -0,0 +1,38 @@
+package nicebank;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountUnit {
+	Account myAccount;
+	
+	@Before
+	public void setUp() throws Exception {
+		myAccount = new Account();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDeposit100() {
+		myAccount.deposit(new Money("$100.00"));
+		
+		Assert.assertEquals(new Money(100, 00), myAccount.getBalance());
+	}
+
+	@Test
+	public void testDebit100from200() {
+		myAccount.deposit(new Money("$200.00"));
+		
+		Assert.assertEquals(new Money(200, 00), myAccount.getBalance());
+		
+		myAccount.debit(100);
+		
+		Assert.assertEquals(new Money(100, 00), myAccount.getBalance());
+	}
+	
+}
