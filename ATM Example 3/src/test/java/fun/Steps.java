99b6dd6a9c08cdf43dbff25bd0a1dc8cc592ec2e
Brian Sjoberg <brian_sjoberg@yahoo.com>
2019-01-10 08:09:51 -0500

    deposit implemented for account.

--- ATM Example 3/src/test/java/fun/Steps.java
+++ ATM Example 3/src/test/java/fun/Steps.java
@@ -6,10 +6,16 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Steps {
+	class Account {
+		void deposit(int amount) {
+			
+		}
+	}
+	
 	@Given("^I have deposited \\$(\\d+) in my account$")
-	public void iHaveDeposited$InMyAccount(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void iHaveDeposited$InMyAccount(int amount) throws Throwable {
+	    Account myAccount = new Account();
+	    myAccount.deposit(amount);
 	}
 
 	@When("^I request \\$(\\d+)$")
