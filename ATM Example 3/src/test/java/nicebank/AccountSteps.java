3a7e0e278d6de0ad7bf4948bd57d58b56d6799f4
Brian Sjoberg <brian_sjoberg@yahoo.com>
2019-01-16 06:59:14 -0500

    Adding step to check balance after withdraw.

--- ATM Example 3/src/test/java/nicebank/AccountSteps.java
+++ ATM Example 3/src/test/java/nicebank/AccountSteps.java
@@ -2,8 +2,10 @@ package nicebank;
 
 import org.junit.Assert;
 
+import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import support.KnowsTheDomain;
 import transforms.MoneyConverter;
 
@@ -19,5 +21,11 @@ public class AccountSteps {
 	    helper.getMyAccount().deposit(amount);
 	    
 	    Assert.assertEquals("Incorrect Account Balance - ", amount, helper.getMyAccount().getBalance());
+	}
+	
+	@Then("^the balance of my account should be \\$(\\d+)\\.(\\d+)$")
+	public void theBalanceOfMyAccountShouldBe$(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
 	}
 }
--- ATM Example 3/src/test/resources/cash_withdrawal.feature
+++ ATM Example 3/src/test/resources/cash_withdrawal.feature
@@ -3,6 +3,7 @@ Feature: Cash Withdrawal
 		Given I have deposited $100.00 in my account
 		When I withdraw $20
 		Then $20 should be dispensed
+		And the balance of my account should be $80.00
 		
 	Scenario: Insufficient when withdraw from a low balance account
 		Given I have deposited $50.00 in my account
