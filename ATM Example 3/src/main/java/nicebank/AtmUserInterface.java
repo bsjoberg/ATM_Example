7713bd4a93dfece420b3de87a6110f3bbc8325b1
Brian Sjoberg <brian_sjoberg@yahoo.com>
2019-01-27 10:37:43 -0500

    Getting the Interface up. ServerHooks for starting and stopping server

--- ATM Example 3/src/main/java/nicebank/AtmUserInterface.java
+++ ATM Example 3/src/main/java/nicebank/AtmUserInterface.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import hooks.ServerHooks;
+
 public class AtmUserInterface implements Teller {
 
 	private final EventFiringWebDriver webDriver;
@@ -15,7 +17,7 @@ public class AtmUserInterface implements Teller {
 	
 	public void withdrawFrom(Account account, int dollars) {
 		try {
-			webDriver.get("http://localhost:9988");
+			webDriver.get("http://localhost:" + ServerHooks.PORT);
 			webDriver.findElement(By.id("amount"))
 				.sendKeys(String.valueOf(dollars));
 			webDriver.findElement(By.id("withdraw")).click();
